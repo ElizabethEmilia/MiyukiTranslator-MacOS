48c6b327ee1cff0ce02f4fe225d429570cd41709
Miyuki <listencpp@gmail.com>
2020-12-30 15:33:08 +0800

    BUG FIX: change UI in main thread

--- Translator/MainWindowContentViewController.swift
+++ Translator/MainWindowContentViewController.swift
@@ -27,6 +27,10 @@ class MainWindowContentViewController: NSViewController {
         view.window?.level = .floating
     }
     
+    var shouldUpdateUI = false;
+    var translatedResultToUpdate = "";
+    var textToTranslateToUpdate = "";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
@@ -41,6 +45,35 @@ class MainWindowContentViewController: NSViewController {
 </html>
 """.data(using: String.Encoding.utf8);
         txtResultDisplay.textStorage!.setAttributedString(NSAttributedString(html: welcomeHTML, documentAttributes: nil)!)
+        
+        // Timer to update UI
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { (t) in
+            if !self.shouldUpdateUI {
+                return
+            }
+            self.shouldUpdateUI = false
+            // check theme
+            let currentStyle = InterfaceStyle()
+            let fontColor = currentStyle == InterfaceStyle.Light ? "#000" : "#fff"
+            
+            let resultHTML:Data! = """
+    <html>
+    <head>
+        <meta charset="utf-8"/>
+        <style>pre { font-family: Times, 'Times New Roman', 'SongTi SC'; font-size: 15px; }</style>
+    </head>
+    <body style="font-family: Times, 'Times New Roman', 'SongTi SC'; color: #ff6699; font-size: 15px;">
+        <p style="">TRANSLATED TEXT:</p>
+        <pre style="color: \(fontColor)">\(self.translatedResultToUpdate)</pre>
+        <br/>
+        <p style="">THE ORIGINAL TEXT:</p>
+        <pre style="color: \(fontColor)">\(self.textToTranslateToUpdate)</pre>
+    </body>
+    </html>
+    """.data(using: String.Encoding.utf8);
+            self.txtResultDisplay.textStorage!.setAttributedString(NSAttributedString(html: resultHTML, documentAttributes: nil)!)
+            //self.txtResultDisplay.scrollRangeToVisible(NSRange(location:0, length:0))
+        }
         
         // Set timer to check clipbpard
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (t) in
@@ -89,25 +122,11 @@ class MainWindowContentViewController: NSViewController {
                 let langTo:String = nonAsciiCount > charArr.count / 2 ? "en" : "zh"
                 
                 translateUsingBaiduTranslateAPIAsync(textToTranslate: str, langFrom: "auto", langTo: langTo, appID: "20160628000024160", appKey: "835JS22N3C2PA4Brrrwo", onComplete: { (ret: String) in
-                        let translatedResult = ret.replacingOccurrences(of: "<", with: "&lt;")
+                    let translatedResult = ret.replacingOccurrences(of: "<", with: "&lt;")
                             .replacingOccurrences(of: ">", with: "&gt;")
-                        let resultHTML:Data! = """
-                <html>
-                <head>
-                    <meta charset="utf-8"/>
-                    <style>pre { font-family: Times, 'Times New Roman', 'SongTi SC'; font-size: 15px; }</style>
-                </head>
-                <body style="font-family: Times, 'Times New Roman', 'SongTi SC'; color: #ff6699; font-size: 15px;">
-                    <p style="">TRANSLATED TEXT:</p>
-                    <pre style="color: \(fontColor)">\(translatedResult)</pre>
-                    <br/>
-                    <p style="">THE ORIGINAL TEXT:</p>
-                    <pre style="color: \(fontColor)">\(strToShow)</pre>
-                </body>
-                </html>
-                """.data(using: String.Encoding.utf8);
-                        self.txtResultDisplay.textStorage!.setAttributedString(NSAttributedString(html: resultHTML, documentAttributes: nil)!)
-                        //self.txtResultDisplay.scrollRangeToVisible(NSRange(location:0, length:0))
+                    self.textToTranslateToUpdate = strToShow
+                    self.translatedResultToUpdate = translatedResult
+                    self.shouldUpdateUI = true
                     }
                 )
             }
