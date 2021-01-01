46faf4b5f814692da65d32fa32feaa9e61c6188e
Miyuki <listencpp@gmail.com>
2021-01-01 21:17:59 +0800

    Replace textview to wkwebview

--- Translator/MainWindowContentViewController.swift
+++ Translator/MainWindowContentViewController.swift
@@ -10,7 +10,8 @@ import WebKit
 import AppKit
 
 class MainWindowContentViewController: NSViewController {
-    @IBOutlet var txtResultDisplay: NSTextView!
+    
+    @IBOutlet weak var resultDisplay: WKWebView!
     
     var storedStringInPasteBoard: String = "";
     
@@ -37,15 +38,19 @@ class MainWindowContentViewController: NSViewController {
         // Do view setup here.
         
         // Load initial screen
-        let welcomeHTML:Data! = """
+        resultDisplay.setValue(true, forKey: "drawsTransparentBackground")
+        let welcomeHTML = """
 <html>
-<body style="font-family: Times, 'Times New Roman', 'SongTi SC'; background: #CCC; color: #ff6699; text-align: center;">
-    <p style="font-size: 18px;"><br/><br/><br/><br/><br/><br/><br/><br/>MIYUKI TRANSLATOR<br/><br/><br/><br/><br/><br/><br/><br/></p>
-    <p style="font-size: 12px; color: #888"><br/><br/><br/><br/><br/>BY MIYUKI, IN DECEMBER, 2020</p>
+<body style="font-family: Times, 'Times New Roman', 'SongTi SC'; background: rgba(255, 255, 255, 0); color: #ff6699; text-align: center; -webkit-user-select: none; cursor: default !important;">
+    <div style="font-size: 18px; position: absolute; height:80%; width: 100%; top: 0; left: 0; display: flex; justify-content: center; align-items: center; -webkit-user-select: none;">
+        <p>MIYUKI TRANSLATOR</p>
+    </div>
+    <p style="font-size: 12px; color: #888; position: absolute; bottom: 10%; left: 0; width: 100%; -webkit-user-select: none;">BY MIYUKI, IN DECEMBER, 2020</p>
+    <script>document.body.setAttribute('oncontextmenu', 'event.preventDefault();');</script>
 </body>
 </html>
-""".data(using: String.Encoding.utf8);
-        txtResultDisplay.textStorage!.setAttributedString(NSAttributedString(html: welcomeHTML, documentAttributes: nil)!)
+"""
+        resultDisplay.loadHTMLString(welcomeHTML, baseURL: nil)
         
         // Timer to update UI
         Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { (t) in
@@ -56,23 +61,25 @@ class MainWindowContentViewController: NSViewController {
             // check theme
             let currentStyle = InterfaceStyle()
             let fontColor = currentStyle == InterfaceStyle.Light ? "#000" : "#fff"
+            let backColor = currentStyle == InterfaceStyle.Light ? "(255,255,255,0.2)" : "(0,0,0,0.2)"
             
-            let resultHTML:Data! = """
+            let resultHTML = """
     <html>
     <head>
         <meta charset="utf-8"/>
-        <style>pre { font-family: Times, 'Times New Roman', 'SongTi SC'; font-size: 15px; }</style>
+        <style>pre { -webkit-user-select: text !important; cursor:text; white-space: pre-wrap;  border-radius: 9px; background: rgba\(backColor); padding: 10px; font-family: Times, 'Times New Roman', 'SongTi SC'; font-size: 15px;  word-wrap:break-word; }</style>
     </head>
-    <body style="font-family: Times, 'Times New Roman', 'SongTi SC'; color: #ff6699; font-size: 15px;">
+    <body style="font-family: Times, 'Times New Roman', 'SongTi SC'; color: #ff6699; font-size: 15px; -webkit-user-select: none; cursor: default; padding: 8px;">
         <p style="">TRANSLATED TEXT:</p>
         <pre style="color: \(fontColor)">\(self.translatedResultToUpdate)</pre>
         <br/>
         <p style="">THE ORIGINAL TEXT:</p>
         <pre style="color: \(fontColor)">\(self.textToTranslateToUpdate)</pre>
+        <script>document.body.setAttribute('oncontextmenu', 'event.preventDefault();');</script>
     </body>
     </html>
-    """.data(using: String.Encoding.utf8);
-            self.txtResultDisplay.textStorage!.setAttributedString(NSAttributedString(html: resultHTML, documentAttributes: nil)!)
+    """
+            self.resultDisplay.loadHTMLString(resultHTML, baseURL: nil)
             //self.txtResultDisplay.scrollRangeToVisible(NSRange(location:0, length:0))
         }
         
@@ -92,6 +99,7 @@ class MainWindowContentViewController: NSViewController {
                 // check theme
                 let currentStyle = InterfaceStyle()
                 let fontColor = currentStyle == InterfaceStyle.Light ? "#000" : "#fff"
+                let backColor = currentStyle == InterfaceStyle.Light ? "(255,255,255,0.2)" : "(0,0,0,0.2)"
                 
                 self.storedStringInPasteBoard = str;
                 let strToShow = str.replacingOccurrences(of: "\r", with: "")
@@ -100,22 +108,24 @@ class MainWindowContentViewController: NSViewController {
                     .replacingOccurrences(of: "<", with: "&lt;")
                     .replacingOccurrences(of: ">", with: "&gt;")
                 
-                let resultHTML:Data! = """
-        <html>
-        <head>
-            <meta charset="utf-8"/>
-            <style>pre { font-family: Times, 'Times New Roman', 'SongTi SC'; font-size: 15px; }</style>
-        </head>
-        <body style="font-family: Times, 'Times New Roman', 'SongTi SC'; color: #ff6699; font-size: 15px;">
-            <p style="">TRANSLATING...</p>
-            <br/>
-            <p style="">THE ORIGINAL TEXT:</p>
-            <pre style="color: \(fontColor)">\(strToShow)</pre>
-        </body>
-        </html>
-        """.data(using: String.Encoding.utf8);
-                self.txtResultDisplay.textStorage!.setAttributedString(NSAttributedString(html: resultHTML, documentAttributes: nil)!)
-                self.txtResultDisplay.scrollRangeToVisible(NSRange(location:0, length:0))
+                let resultHTML = """
+    <html>
+    <head>
+        <meta charset="utf-8"/>
+        <style>pre { -webkit-user-select: text !important; cursor:text; white-space: pre-wrap;  border-radius: 9px; background: rgba\(backColor); padding: 10px; font-family: Times, 'Times New Roman', 'SongTi SC'; font-size: 15px;  word-wrap:break-word; }</style>
+    </head>
+    <body style="font-family: Times, 'Times New Roman', 'SongTi SC'; color: #ff6699; font-size: 15px; -webkit-user-select: none; cursor: default; padding: 8px;">
+        <p style="">TRANSLATING:</p>
+        <pre style="color: \(fontColor)88"><i>Translating, please wait...</i></pre>
+        <br/>
+        <p style="">THE ORIGINAL TEXT:</p>
+        <pre style="color: \(fontColor)">\(self.textToTranslateToUpdate)</pre>
+        <script>document.body.setAttribute('oncontextmenu', 'event.preventDefault();');</script>
+    </body>
+    </html>
+"""
+                self.resultDisplay.loadHTMLString(resultHTML, baseURL: nil)
+            
                 
                 // 判断是应该中文->英语还是英语->中文
                 let charArr = str.unicodeScalars
@@ -138,5 +148,4 @@ class MainWindowContentViewController: NSViewController {
             }
         }
     }
-    
 }
